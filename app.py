758f3f45a05d8ea29f123dcb02e31f884125e48c
Amit Halder <amith3169@gmail.com>
2026-04-15 13:50:36 +0530

    update

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ from streamlit.web import cli as stcli
 
 if __name__ == "__main__":
     # Launch the Streamlit UI
-    ui_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "ui.py")
+    ui_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), "streamlitui.py")
     sys.argv = ["streamlit", "run", ui_path, "--server.port=8080"]
     stcli.main()
